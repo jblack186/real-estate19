80d980c8b6ef221d772b50e9da97ca3635feba2b
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-01-24 03:48:01 -0500

    styled comment section

--- real-estate/src/MidSection.js
+++ real-estate/src/MidSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './MidSection.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 import Wall from './wall.jpg';
 import Building from './building.jpg';
 import {Container, Row, Col, Button} from 'react-bootstrap';
@@ -7,6 +9,9 @@ import Agent from './agent.jpg';
 import Agent1 from './agent001.jpg';
 import Agent2 from './agent002.jpg';
 import Meet from './meet.png';
+import Featured from './featured.png';
+import House2 from './house2.jpg';
+
 
 const MidSection = () => {
   
@@ -33,8 +38,34 @@ const MidSection = () => {
 
       </div>
       <Button size="lg" variant="outline-light">READ MORE</Button>
-
       </div>
+      <div className='comment-icon'>
+      <FontAwesomeIcon style={{color: '#bba462', fontSize: '3rem'}} icon={faQuoteRight} />
+    </div>
+        <div className='comments'>
+        <p>Brandon’s commitment, professionalism, knowledge, and loyalty has surpassed all of my expectations. He builds personal relationships with all of his clients and keeps their wants and needs an upmost priority. His honesty and persistence is what I appreciate the most, as you can put 100% of your trust in him as a realtor. I could not be happier with my experience with Brandon and will definitely keep him as my #1 in future endeavors!</p>
+        <Button size="lg" variant="outline-primary">READ MORE</Button>
+      </div>
+
+      <div className='featured-container'>
+      <Container>
+  <Row className='rows'>
+    <Col className='columns' lg={6}>
+      
+      <img className='featured-logo' src={Featured} alt='featured log' />
+     
+      <p>We are a full service Real Estate Team serving the Atlanta Metro area. We Specialize in Cobb, Paulding, Atlanta and surrounding areas. Buying, Selling, and Staging are just a few of our services.</p>
+<p>
+Our team of experts represents the best and brightest in the industry, and we’re always striving to lead the field in research, innovation, and consumer education. Today’s buyers and sellers need a trusted resource that can guide them through the complex world of real estate. With our extensive knowledge and commitment to providing only the best and most timely information to our clients, we are your go-to source for real estate industry insight and advice.</p>
+<Button size="lg" variant="outline-dark">VIEW MORE</Button>
+    </Col>
+    <Col className='columns' lg={6}>
+      <img className='agent' src={House2} alt='realestate agent' />
+    </Col>
+  </Row>
+</Container>
+</div>
+
    </section>
   )
 }
